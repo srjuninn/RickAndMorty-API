c624806798d80e497fcc89c983122ebdd41c4c97
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-04-07 11:15:32 -0300

    feat: add rick and morty api

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Header } from './components/Header'
+import { Main } from './components/Main'
+import { CharactersSection } from './components/SectionCharacters'
+import { Footer } from './components/Footer'
+
+export default function App() {
+  return (
+    <>
+    <Header/>
+    <Main/>
+    <CharactersSection/>
+    <Footer/>
+    </>
+  )
+}
