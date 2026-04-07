7ea23aac641db1479a1ad8f184303fe86a4c44c2
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-04-07 11:14:10 -0300

    style: add footer

---
+++ src/components/FooterStyle.js
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+export const StyledFooter =styled.footer`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 60px 0;
+    background-color: var(--color-black);
+
+    h2{
+        font-size: 20px;
+        font-weight: bold;
+        color: var(--neutral-white);
+
+        span{
+            color: var(--color-primary);
+        }
+    }
+`   
