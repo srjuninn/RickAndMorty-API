a5bee45f99ce391a5b4a4cb56e28f794c07e0c78
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-04-14 10:36:50 -0300

    feat: add section characters

--- src/components/SectionCharacters.jsx
+++ src/components/SectionCharacters.jsx
@@ -1,8 +1,23 @@
 import { Card } from "./Card"
 import { StyledCharacterSection } from "./SectionCharacterStyle"
-
+import { useEffect, useState } from "react"
+import axios from 'axios'
 
 export const CharactersSection = () => {
+
+    const [characters, setCharacters] = useState([])
+
+    useEffect(() => {
+        axios.get("https://rickandmortyapi.com/api/character")
+            .then(res => {
+                console.log(res.data.results)
+                setCharacters(res.data.results)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }, [])
+    
     return(
         <StyledCharacterSection>
             <h2>Nossos <span>Personagens</span></h2>
@@ -10,8 +25,16 @@ export const CharactersSection = () => {
                {/* aqui será nosso componente dos cards */}
                {/* substituir essa div de baixo pelo card
                 component que vou criar */}
-                <div className="cards"></div>
-                <Card/>
+                {characters.map((character) => (
+                    <Card 
+                        img={character.image}
+                        name={character.name}
+                        specie={character.species}
+                        status={character.status}
+                        location={character.origin.name}
+                    />
+                ))}
+
             </div>
         </StyledCharacterSection>
     )
